5dab916dfa6ca13457fe45a7c1ef67c798322da5
Michelle Flores <lmfg2456@gmail.com>
2022-03-28 08:36:46 -0700

    created items schema

---
+++ box-heads-back-end/models/items.jsx
@@ -0,0 +1,14 @@
+const { Schema } = require('mongoose')
+
+
+const Item = new Schema(
+    {
+      name: { type: String, required:true }, 
+      id: { type: Number, required:true },
+      price: { type: Number, required: true },
+      description: { type: String, required: true},
+      image: { type: String, required: true}
+    },
+    {timestamps: true }
+)   
+    module.exports = Item
