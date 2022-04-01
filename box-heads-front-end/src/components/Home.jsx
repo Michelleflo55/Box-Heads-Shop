3dd9bb214b0b59b83b67b6c36083fa0fed61c338
Michelle Flores <lmfg2456@gmail.com>
2022-04-01 08:16:22 -0700

    flex home

--- box-heads-front-end/src/components/Home.jsx
+++ box-heads-front-end/src/components/Home.jsx
@@ -20,18 +20,21 @@ function Home(props) {
     }, []) 
   
   return (
-    <div className="flex">
-        <div className="left-column">
-            <h1 className='nft'>Learn about NFTS</h1>   
-        </div>
-        <div className="middle-column">
-            <img src="https://i.ibb.co/QPDK3VD/LOGO.png" className="boxHead" alt="logo"/>
-        </div>
-        <div className='right-column shop'>
-          <h1>
-            <Link to="/items" >Shop</Link>
-          </h1>
-        </div>
+    <div>
+      <header>Welcome</header>
+      <div className="flex">
+          <div className="left-column">
+              <h1 className='nft'>Learn about NFTS</h1>   
+          </div>
+          <div className="middle-column">
+              <img src="https://i.ibb.co/QPDK3VD/LOGO.png" className="boxHead" alt="logo"/>
+          </div>
+          <div className='right-column shop'>
+            <h1>
+              <Link to="/items" >Shop</Link>
+            </h1>
+          </div>
+      </div>
     </div>
   )
 }
